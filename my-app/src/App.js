05163410d505453d88a49c8926ae72497079ab2a
0036ak <aankushkamboj033@mail.com>
2024-08-27 12:28:31 +0530

    create react app

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className='header'>
+        <header>
+          
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
